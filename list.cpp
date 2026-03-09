4587d308e8bcee815b380e01d9949fe024c99aab
anand3232 <shukla.anand1@gmail.com>
2026-03-09 16:23:17 +0530

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,2 +1,25 @@
 #include<list.h>
 
+List::List(Node *h=nullptr):head(h){}
+void List::add(int data)
+{
+    Node *newNode;
+    if(head==nullptr)
+    {
+      newNode= new Node;
+      newNode->data=data;
+      newNode->next=nullptr;
+      head=newNode;
+      return;
+    }
+    Node *temp=head
+    while(temp->next!=nullptr;
+      temp=temp->next;
+      newNode=new Node;
+      newNode->data=data;
+      newNode->next=nullptr;
+      temp->next=newNode;
+    }
+}
+
+
