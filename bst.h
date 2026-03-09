541434df4669c7309d44aa02143ac7abc24db985
anand421 <shukla.anand1@gmail.com>
2026-03-09 16:54:13 +0530

    Added code in bst.h

--- bst.h
+++ bst.h
@@ -1,1 +1,21 @@
+//Create a binary tree header file
 
+#ifndef BINARY_H
+#define BINARY_H
+
+struct Node
+{
+   Node *lnext;
+   int data;
+   Node *rnext;
+};
+
+class BST
+{
+   private:
+	Node *root;
+   public:
+	void insert(int data)
+	void inorder();
+
+};
